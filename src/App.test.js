233f5149704aebfdfd9b1344d24d1b112ce74739
Luis Deschamps Rudge <luis@luisrudge.net>
2018-04-12 11:10:05 -0300

    Fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import reducers from "../src/reducers";
+
+const store = createStore(reducers, applyMiddleware(reduxThunk));
+
+import App from "./App";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
